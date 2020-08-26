cb57c89607c840fcc9a39b6a095d2d915e27e11d
filipewk <filipewk@gmail.com>
2020-08-26 15:54:03 -0300

    refactor: create configs to xpath

--- src/main/bot/generate-gnre.ts
+++ src/main/bot/generate-gnre.ts
@@ -1,8 +1,12 @@
-﻿import { Builder, By } from 'selenium-webdriver'
-import { ChromeGnre, CliComandsParams } from '../../domain/usecases/gnre/generate-gnre'
+﻿import { ChromeGnre, CliComandsParams } from '../../domain/usecases/gnre/generate-gnre'
 import { ResultIcmsSt } from '../calculate/result-icms-st'
+import { xPathContents, links } from '../configs/contents'
 import { ReadXml } from '../../domain/usecases/xml/read-xml'
-const driver = new Builder().forBrowser('chrome').build()
+import { Builder, By } from 'selenium-webdriver'
+
+const driver = new Builder()
+  .forBrowser('chrome')
+  .build()
 
 export class GenerateGnre implements ChromeGnre {
   constructor (
@@ -12,26 +16,41 @@ export class GenerateGnre implements ChromeGnre {
 
   async generate (cliComandsParams: CliComandsParams): Promise<void> {
     try {
+      const { mva12, mva4, aliquotaInterna, numeroNfe, dataVencimento } = cliComandsParams
+      const { nIe, nNFe, chaveNfe } = this.convertedJson.readXml(numeroNfe)
+      const dateWithNoBar = dataVencimento.replace('/', '')
+      const totalWithNoComma = this.resultSt.result({
+        mva12,
+        mva4,
+        aliquotaInterna,
+        numeroNfe
+      }).replace('.', '')
+
       const documentInitialised = async () =>
         await driver.executeScript('return document.readyState;')
 
-      await driver.get('https://www.arinternet.pr.gov.br/portalsefa/_d_gnre1.asp')
-      await driver.findElement(By.xpath("//select[@name='eCodigo']")).sendKeys('100099,10009-9 ICMS - SUBST.TRIBUTÁRIA POR OPERAÇÃO')
-      await driver.findElement(By.xpath("//input[@name='eInscricao']")).sendKeys(this.convertedJson.readXml(cliComandsParams.numeroNfe).nIe.toString())
-      await driver.findElement(By.xpath("//input[@value='Consultar']")).click()
+      await driver.get(links.gnreUrl)
+      await driver.findElement(By.xpath(xPathContents.dropdownGnre))
+        .sendKeys('100099,10009-9 ICMS - SUBST.TRIBUTÁRIA POR OPERAÇÃO')
+      await driver.findElement(By.xpath(xPathContents.inscricaoEstadual))
+        .sendKeys(nIe.toString())
+      await driver.findElement(By.xpath(xPathContents.buttonConsultar))
+        .click()
 
-      // selenium open new page
-      const dateWithNoBar = cliComandsParams.dataVencimento.replace('/', '')
-      const { mva12, mva4, aliquotaInterna, numeroNfe } = cliComandsParams
-      const totalWithNoComma = this.resultSt.result({ mva12, mva4, aliquotaInterna, numeroNfe }).replace('.', '')
+      // new page
       await driver.wait(async () => documentInitialised(), 10000)
-      await driver.findElement(By.xpath("//input[@name='eNumDoc']")).sendKeys(this.convertedJson.readXml(cliComandsParams.numeroNfe).nNFe.toString())
-      await driver.findElement(By.xpath("//input[@name='Campo06']")).sendKeys(totalWithNoComma)
-      await driver.findElement(By.xpath("//input[@name='DtVencimento']")).sendKeys(dateWithNoBar)
-      await driver.findElement(By.xpath("//select[@name='eDiaDtCalculo']")).sendKeys(dateWithNoBar.slice(0, 2))
-      await driver.findElement(By.xpath("//input[@name='b1']")).click()
+      await driver.findElement(By.xpath(xPathContents.numeroDoc))
+        .sendKeys(nNFe.toString())
+      await driver.findElement(By.xpath(xPathContents.totalSt))
+        .sendKeys(totalWithNoComma)
+      await driver.findElement(By.xpath(xPathContents.dataVencimentoGnre))
+        .sendKeys(dateWithNoBar)
+      await driver.findElement(By.xpath(xPathContents.dropdownDiaVencimento))
+        .sendKeys(dateWithNoBar.slice(0, 2))
+      await driver.findElement(By.xpath(xPathContents.buttonNext))
+        .click()
 
-      // handle new tab
+      // get new tab
       const tab = await driver.wait(
         async () => (await driver.getAllWindowHandles()).length === 2,
         10000
@@ -45,17 +64,19 @@ export class GenerateGnre implements ChromeGnre {
             await driver.switchTo().window(handle)
           }
         })
-        await driver.findElement(By.xpath("//textarea[@name='Campo23']"))
-          .sendKeys(`ICMS ST s/ NOTA FISCAL DE COMPRA,\nChave NFE: ${this.convertedJson.readXml(cliComandsParams.numeroNfe).chaveNfe.replace('NFe', '')}`)
-        await driver.findElement(By.xpath("//input[@name='b1']")).click()
+        await driver.findElement(By.xpath(xPathContents.observacoes))
+          .sendKeys(`ICMS ST s/ NOTA FISCAL DE COMPRA,\nChave NFE: ${chaveNfe.replace('NFe', '')}`)
+        await driver.findElement(By.xpath(xPathContents.buttonNext))
+          .click()
       }
 
       await driver.sleep(3000)
       const getWindow = await driver.getAllWindowHandles()
       await driver.switchTo().window(getWindow[3])
-      const iframe = await driver.findElement(By.xpath("//frame[@name='header']"))
+      const iframe = await driver.findElement(By.xpath(xPathContents.frameToPrint))
       await driver.switchTo().frame(iframe)
-      await driver.findElement(By.xpath("//input[@value=' Imprimir ']")).click()
+      await driver.findElement(By.xpath(xPathContents.buttonPrint))
+        .click()
       await driver.sleep(300000)
     } catch (error) {
       console.log(error)
---
+++ src/main/configs/contents.ts
@@ -0,0 +1,17 @@
+﻿export const links = {
+  gnreUrl: 'https://www.arinternet.pr.gov.br/portalsefa/_d_gnre1.asp'
+}
+
+export const xPathContents = {
+  dropdownGnre: "//select[@name='eCodigo']",
+  inscricaoEstadual: "//input[@name='eInscricao']",
+  buttonConsultar: "//input[@value='Consultar']",
+  numeroDoc: "//input[@name='eNumDoc']",
+  totalSt: "//input[@name='Campo06']",
+  dataVencimentoGnre: "//input[@name='DtVencimento']",
+  dropdownDiaVencimento: "//select[@name='eDiaDtCalculo']",
+  buttonNext: "//input[@name='b1']",
+  observacoes: "//textarea[@name='Campo23']",
+  frameToPrint: "//frame[@name='header']",
+  buttonPrint: "//input[@value=' Imprimir ']"
+}
