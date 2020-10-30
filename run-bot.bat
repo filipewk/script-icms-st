9dc3865f8806d095ffc99f850d8714ea92611ef2
Filipe A. Oliveira <filipewk@gmail.com>
2020-10-30 16:18:59 -0300

    update run-bot.bat

--- run-bot.bat
+++ run-bot.bat
@@ -2,6 +2,15 @@ SET /P numeroNfe= Digite o numero da Nfe:
 SET /P mva12= Digite a aliquota da mva a 12 se deixar em branco ficara por padrao 84,35: 
 SET /P mva4= Digite a aliquota da mva a 4 se deixar em branco ficara por padrao 101,11:
 SET /P aliquotaInterna= Digite a aliquota interna do estado, se deixar em branco ficara por padrao 18:
-SET /P dataVencimento= Digite a data de vencimento da GNRE: 
+SET /P gerarGnre= Digite S para sim e N para nao:
 
-node dist/cli  -gnre -a %numeroNfe% -d %dataVencimento% -m12 %mva12% -m4 %mva4% -al %aliquotaInterna%
+IF "%gerarGnre%"=="s" (
+SET /P dataVencimento= Digite a data de vencimento da GNRE:
+)
+
+IF "%gerarGnre%"=="n" (
+    node dist/cli -a %numeroNfe% -m12 %mva12% -m4 %mva4% -al %aliquotaInterna%
+pause
+) ELSE (
+node dist/cli -gnre -a %numeroNfe% -d %dataVencimento% -m12 %mva12% -m4 %mva4% -al %aliquotaInterna%
+)
